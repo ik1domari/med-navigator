ca2de954f93974c00436ff3380aa790635db9a27
Timur <astominsflavadept@gmail.com>
2024-03-13 21:04:39 +0500

    v0.0.3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,33 @@ export default function Header() {
 	return (
 		<header className='header'>
 			<h1 className='header__logo'>МЕДИЦИНСКИЙ НАВИГАТОР</h1>
+			<nav className='header__nav'>
+				<a href='#' className='header__navlink'>
+					О ПРОДУКТЕ
+				</a>
+				<a href='#' className='header__navlink'>
+					ФУНКЦИИ БОТА
+				</a>
+				<a href='#' className='header__navlink'>
+					ЦЕНА
+				</a>
+			</nav>
+			<a href='#' className='header__link'>
+				КОНТАКТЫ
+				<svg
+					width='16'
+					height='16'
+					viewBox='0 0 22 21'
+					fill='none'
+					xmlns='http://www.w3.org/2000/svg'
+				>
+					<path
+						d='M1 1L21 20M21 20V1M21 20H1'
+						stroke='#EFEFEF'
+						strokeWidth='2'
+					/>
+				</svg>
+			</a>
 		</header>
 	);
 }
